78a3afccb99e38e5b756202e67051ae26961646a
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-09-17 14:45:03 +0200

    `Hypixel` Added `getGuild()`

--- packages/hypixel/src/index.ts
+++ packages/hypixel/src/index.ts
@@ -1,6 +1,6 @@
 import { parseUUID } from "@pixelic/mojang";
 import { requestHypixel } from "./requestHandler.js";
-import { formatPlayer } from "./formatters.js";
+import { formatPlayer, formatGuild } from "./formatters.js";
 
 export const getPlayer = async (player: string) => {
   const UUID = await parseUUID(player);
@@ -12,4 +12,39 @@ export const getPlayer = async (player: string) => {
   } catch {
     return null;
   }
+};
+
+export const getGuild = async ({ player, ID, name }: { player?: string; ID?: string; name?: string }) => {
+  if (player) {
+    const UUID = await parseUUID(player);
+    if (UUID === null) return "Invalid UUID or Username";
+    try {
+      const data = await requestHypixel(`https://api.hypixel.net/guild?player=${UUID}`);
+      if (data.guild === null) return "This player is not in a Guild";
+      return formatGuild(data.guild);
+    } catch {
+      return null;
+    }
+  }
+  if (ID) {
+    if (!/^[0-9a-fA-F]{24}$/.test(ID)) return "Invalid Guild ID";
+    try {
+      const data = await requestHypixel(`https://api.hypixel.net/guild?id=${ID}`);
+      if (data.guild === null) return "This Guild does not exist";
+      return formatGuild(data.guild);
+    } catch {
+      return null;
+    }
+  }
+  if (name) {
+    if (!/^[a-zA-Z0-9_]{2,32}$/.test(name)) return "Invalid Guild Name";
+    try {
+      const data = await requestHypixel(`https://api.hypixel.net/guild?name=${name}`);
+      if (data.guild === null) return "This Guild does not exist";
+      return formatGuild(data.guild);
+    } catch {
+      return null;
+    }
+  }
+  return null;
 };
