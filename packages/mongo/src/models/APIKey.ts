1740b1fc7352b91e79f7a36e799250d631541e13
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-09-28 15:37:05 +0200

    `Mongo` Better `APIKey` Model

--- packages/mongo/src/models/APIKey.ts
+++ packages/mongo/src/models/APIKey.ts
@@ -2,6 +2,18 @@ import { Schema } from "mongoose";
 import { client } from "../index.js";
 import { APIKeyMongo } from "@pixelic/types";
 
+const requestSchema = new Schema(
+  {
+    ID: { type: String, index: true },
+    URL: { type: String },
+    method: { type: String, enum: ["GET", "POST", "PUT", "PATCH", "DELETE"] },
+    userAgent: { type: String },
+    IP: { type: String },
+    key: { type: String },
+  },
+  { _id: false }
+);
+
 const keySchema = new Schema<APIKeyMongo>({
   owner: {
     type: String,
@@ -11,17 +23,7 @@ const keySchema = new Schema<APIKeyMongo>({
   usageHistory: {
     type: Object,
   },
-  requestHistory: [
-    {
-      ID: { type: String, index: true },
-      URL: { type: String },
-      method: { type: String, enum: ["GET", "POST", "PUT", "PATCH", "DELETE"] },
-      userAgent: { type: String },
-      IP: { type: String },
-      key: { type: String },
-    },
-    { _id: false },
-  ],
+  requestHistory: [requestSchema],
 });
 
 export const APIKeyModel = client.useDb("API").model<APIKeyMongo>("API-Key", keySchema);
