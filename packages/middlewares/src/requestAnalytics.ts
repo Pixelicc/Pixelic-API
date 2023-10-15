2937237a1574ab6f6e568087297c7be112722ba4
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-10-15 17:56:52 +0200

    `Middlewares` `requestAnalytics()` now tracking requests per day

--- packages/middlewares/src/requestAnalytics.ts
+++ packages/middlewares/src/requestAnalytics.ts
@@ -7,6 +7,7 @@ import redis from "@pixelic/redis";
 export const requestAnalytics = async (req: Request, res: Response, next: NextFunction) => {
   try {
     await redis.incr("API:Analytics:Requests");
+    await redis.hincrby("API:Analytics:RequestsHistory", new Date().toISOString().slice(0, 10), 1);
     /**
      * Add further Analytics if you want to track more precise data like User-Agents
      */
