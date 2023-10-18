0222a663b6701880bffc10099a74ecdb2bd0df2c
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-10-18 17:45:17 +0200

    `Mongo` Added logging of Connection Status

--- packages/mongo/src/connect.ts
+++ packages/mongo/src/connect.ts
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
+import log from "@pixelic/logger";
 import { config } from "@pixelic/utils";
 
 mongoose.pluralize(null);
 
 const connection = mongoose.createConnection(config.database.mongoDB);
 connection.setMaxListeners(0);
+connection.once("connected", () => log("Mongo", "Connection established", "info"));
+connection.on("disconnected", () => log("Mongo", "Connection closed", "error"));
+connection.on("reconnected", () => log("Mongo", "Connection reestablished", "info"));
 
 export const client = connection;
