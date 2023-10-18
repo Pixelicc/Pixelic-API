c994bded98a7cfbfe0e5c3b12da36c1a7160265d
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-10-18 18:30:08 +0200

    `Mongo` Added ability to reconnect
    - Also swapped to default mongoose connection

--- packages/mongo/src/connect.ts
+++ packages/mongo/src/connect.ts
@@ -4,10 +4,24 @@ import { config } from "@pixelic/utils";
 
 mongoose.pluralize(null);
 
-const connection = mongoose.createConnection(config.database.mongoDB);
-connection.setMaxListeners(0);
-connection.once("connected", () => log("Mongo", "Connection established", "info"));
-connection.on("disconnected", () => log("Mongo", "Connection closed", "error"));
-connection.on("reconnected", () => log("Mongo", "Connection reestablished", "info"));
+var initalConnect = true;
+const connect = async () => {
+  try {
+    await mongoose.connect(config.database.mongoDB, { connectTimeoutMS: 5000 });
+    initalConnect = false;
+  } catch {
+    if (initalConnect) log("Mongo", "Connection not establishable", "error");
+    connect();
+  }
+};
 
-export const client = connection;
+mongoose.connection.setMaxListeners(0);
+mongoose.connection.once("connected", () => log("Mongo", "Connection established", "info"));
+mongoose.connection.on("disconnected", () => {
+  if (!initalConnect) log("Mongo", "Connection closed", "error");
+});
+mongoose.connection.on("reconnected", () => log("Mongo", "Connection reestablished", "info"));
+
+connect();
+
+export const client = mongoose.connection;
