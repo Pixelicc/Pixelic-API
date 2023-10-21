85f22ea1cdc35e8e1d0b78c57c345c012944da0e
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-10-21 14:51:39 +0200

    `Utils` Fixed `Validators` on unexpected datatypes

--- packages/utils/src/validators.ts
+++ packages/utils/src/validators.ts
@@ -1,12 +1,18 @@
 import { formatUUID } from "./formatters.js";
 
 export const validateUUID = (UUID: string) => {
-  if (UUID === undefined) return false;
+  if (typeof UUID !== "string") return false;
   UUID = formatUUID(UUID);
   if (UUID.length !== 32) return false;
   return /[0-9a-f]{12}4[0-9a-f]{19}/.test(UUID);
 };
 
-export const validateUsername = (username: string) => /^[a-zA-Z0-9_]{2,16}$/.test(username);
+export const validateUsername = (username: string) => {
+  if (typeof username !== "string") return false;
+  return /^[a-zA-Z0-9_]{2,16}$/.test(username);
+};
 
-export const validateSkyblockItemID = (ID: string) => /^[A-Z\d\_:]+$/.test(ID);
+export const validateSkyblockItemID = (ID: string) => {
+  if (typeof ID !== "string") return false;
+  return /^[A-Z\d\_:]+$/.test(ID);
+};
