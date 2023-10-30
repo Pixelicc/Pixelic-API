5a7f9db4225500bb8baaa8047d8d1e256214665f
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-10-30 18:40:47 +0100

    `Utils` Added Logging to `SLPPing()`

--- packages/utils/src/SLPPing.ts
+++ packages/utils/src/SLPPing.ts
@@ -1,12 +1,16 @@
 import mc, { NewPingResult } from "minecraft-protocol";
+import log from "@pixelic/logger";
 
 /**
  * Send a Minecraft Sever List Ping (SLP) to the specified host and port
  */
 export const sendSLP = async (host: string, port: string | number) => {
   try {
-    return (await mc.ping({ host, port: Number(port) })) as NewPingResult;
+    const SLPData = (await mc.ping({ host, port: Number(port) })) as NewPingResult;
+    log("Utils", `Pinged ${host}:${port} (${SLPData?.latency || -1}ms)`, "info");
+    return SLPData;
   } catch {
+    log("Utils", `Failed to ping ${host}:${port}`, "warn");
     return null;
   }
 };
