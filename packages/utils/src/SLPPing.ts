e1f1b132009c333d67f1bf2d374cb663788d0141
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-10-31 17:13:59 +0100

    `Utils` Added timeout to `sendSLP()`

--- packages/utils/src/SLPPing.ts
+++ packages/utils/src/SLPPing.ts
@@ -4,13 +4,33 @@ import log from "@pixelic/logger";
 /**
  * Send a Minecraft Sever List Ping (SLP) to the specified host and port
  */
-export const sendSLP = async (host: string, port: string | number) => {
-  try {
-    const SLPData = (await mc.ping({ host, port: Number(port) })) as NewPingResult;
-    log("Utils", `Pinged ${host}:${port} (${SLPData?.latency || -1}ms)`, "info");
-    return SLPData;
-  } catch {
-    log("Utils", `Failed to ping ${host}:${port}`, "warn");
-    return null;
+export const sendSLP = async (host: string, port: string | number, options?: { timeout: number }) => {
+  if (options?.timeout) {
+    return Promise.race([
+      new Promise<null>((resolve) =>
+        setTimeout(() => {
+          resolve(null);
+        }, options.timeout)
+      ),
+      new Promise<NewPingResult | null>(async (resolve) => {
+        try {
+          const SLPData = (await mc.ping({ host, port: Number(port) })) as NewPingResult;
+          log("Utils", `Pinged ${host}:${port} (${SLPData?.latency || -1}ms)`, "info");
+          resolve(SLPData);
+        } catch {
+          log("Utils", `Failed to ping ${host}:${port}`, "warn");
+          resolve(null);
+        }
+      }),
+    ]);
+  } else {
+    try {
+      const SLPData = (await mc.ping({ host, port: Number(port) })) as NewPingResult;
+      log("Utils", `Pinged ${host}:${port} (${SLPData?.latency || -1}ms)`, "info");
+      return SLPData;
+    } catch {
+      log("Utils", `Failed to ping ${host}:${port}`, "warn");
+      return null;
+    }
   }
 };
