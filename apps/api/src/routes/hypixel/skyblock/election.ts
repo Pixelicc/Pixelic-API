b93ee8c11ee9cd511a8007c05aa92483f97a0cdc
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-10-31 18:27:46 +0100

    `API` Added Skyblock Election Endpoint

---
+++ apps/api/src/routes/hypixel/skyblock/election.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import * as Sentry from "@sentry/node";
+import { getSkyblockElection } from "@pixelic/hypixel";
+import { HypixelSkyblockElectionModel } from "@pixelic/mongo";
+import { ratelimit } from "@pixelic/middlewares";
+
+const router = express.Router();
+
+router.get("/v1/hypixel/skyblock/election", async (req, res) => {
+  try {
+    res.set("Cache-Control", "public, max-age=3600");
+    return res.json({ success: true, ...(await getSkyblockElection()) });
+  } catch (e) {
+    Sentry.captureException(e);
+    return res.status(500).json({ success: false });
+  }
+});
+
+router.get("/v1/hypixel/skyblock/election/history", ratelimit(), async (req, res) => {
+  try {
+    res.set("Cache-Control", "public, max-age=3600");
+
+    const data = await HypixelSkyblockElectionModel.find({}, ["-__v"]).lean();
+    const formattedData = [];
+    for (const election of data) {
+      formattedData.push({ year: election._id, candidates: election.candidates, timestamp: election.timestamp });
+    }
+
+    return res.json({ success: true, data: formattedData });
+  } catch (e) {
+    Sentry.captureException(e);
+    return res.status(500).json({ success: false });
+  }
+});
+
+export default router;
--- apps/api/src/routes/hypixel/skyblock/index.ts
+++ apps/api/src/routes/hypixel/skyblock/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import auctionhouse from "./auctionhouse.js";
 import bazaar from "./bazaar.js";
 import items from "./items.js";
+import election from "./election.js";
 
 const router = express.Router();
 
-router.use(auctionhouse, bazaar, items);
+router.use(auctionhouse, bazaar, items, election);
 
 export default router;
