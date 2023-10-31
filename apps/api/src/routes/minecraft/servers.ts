b30fb3c680bad706ea547e831951b0e6d9f110a4
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-10-31 21:44:59 +0100

    `API` Fixed `/v1/minecraft/server/*`

--- apps/api/src/routes/minecraft/servers.ts
+++ apps/api/src/routes/minecraft/servers.ts
@@ -1,20 +1,18 @@
 import express from "express";
 import * as Sentry from "@sentry/node";
 import redis from "@pixelic/redis";
-import { formatTimeseries, formatUUID, validateUUID } from "@pixelic/utils";
+import { formatNumber, formatTimeseries, formatUUID, validateUUID } from "@pixelic/utils";
 import { MinecraftServerPlayercountModel } from "@pixelic/mongo";
+import servers from "../../../../collector/dist/minecraft/servers.js";
 
 const router = express.Router();
 
 router.get("/v1/minecraft/server/list", async (req, res) => {
   try {
-    if (!(await redis.exists("Minecraft:serverList"))) return res.status(500).json({ success: false });
-    const serverList: { UUID: string; name: string; host: string; port: string }[] = JSON.parse((await redis.call("JSON.GET", "Minecraft:serverList", "$")) as string)[0];
-
     res.set("Cache-Control", "public, max-age=300");
     return res.json({
       success: true,
-      servers: serverList,
+      servers,
     });
   } catch (e) {
     Sentry.captureException(e);
@@ -24,17 +22,19 @@ router.get("/v1/minecraft/server/list", async (req, res) => {
 
 router.get("/v1/minecraft/server/:server", async (req, res) => {
   try {
-    const serverList: { UUID: string; name: string; host: string; port: string }[] = JSON.parse((await redis.call("JSON.GET", "Minecraft:serverList", "$")) as string)[0];
-    if (!validateUUID(formatUUID(req.params.server)) || !serverList.some((s) => formatUUID(req.params.server) === s.UUID)) return res.status(422).json({ success: false, cause: "Invalid Server UUID" });
+    if (!servers.some((s) => req.params.server === s.ID)) return res.status(422).json({ success: false, cause: "Invalid Server ID" });
 
     res.set("Cache-Control", "public, max-age=60");
-    const SLPData = JSON.parse((await redis.get(`Minecraft:Servers:${formatUUID(req.params.server)}`)) as string);
+    const SLPData = JSON.parse((await redis.get(`Minecraft:Servers:${req.params.server}`)) as string);
 
     return res.json({
       success: true,
       latency: SLPData.latency,
+      latencyFormatted: `${SLPData.latency}ms`,
       maxPlayers: SLPData.players.max,
+      maxPlayersFormatted: formatNumber(SLPData.players.max, 2),
       onlinePlayers: SLPData.players.online,
+      onlinePlayersFormatted: formatNumber(SLPData.players.online, 2),
       MOTD: SLPData.description,
       icon: SLPData.favicon,
     });
@@ -46,8 +46,7 @@ router.get("/v1/minecraft/server/:server", async (req, res) => {
 
 router.get("/v1/minecraft/server/:server/history", async (req, res) => {
   try {
-    const serverList: { UUID: string; name: string; host: string; port: string }[] = JSON.parse((await redis.call("JSON.GET", "Minecraft:serverList", "$")) as string)[0];
-    if (!validateUUID(formatUUID(req.params.server)) || !serverList.some((s) => formatUUID(req.params.server) === s.UUID)) return res.status(422).json({ success: false, cause: "Invalid Server UUID" });
+    if (!servers.some((s) => req.params.server === s.ID)) return res.status(422).json({ success: false, cause: "Invalid Server ID" });
 
     res.set("Cache-Control", "public");
     const date = new Date();
@@ -56,7 +55,7 @@ router.get("/v1/minecraft/server/:server/history", async (req, res) => {
 
     return res.json({
       success: true,
-      data: formatTimeseries(await MinecraftServerPlayercountModel.longTerm.find({ meta: formatUUID(req.params.server) }, ["-meta", "-_id", "-__v"]).lean()),
+      data: formatTimeseries(await MinecraftServerPlayercountModel.longTerm.find({ meta: req.params.server }, ["-meta", "-_id", "-__v"]).lean()),
     });
   } catch (e) {
     Sentry.captureException(e);
@@ -67,8 +66,7 @@ router.get("/v1/minecraft/server/:server/history", async (req, res) => {
 router.get("/v1/minecraft/server/:server/history/:timeframe", async (req, res) => {
   try {
     if (!["hour", "day", "week", "month", "year"].includes(req.params.timeframe)) return res.status(422).json({ success: false, cause: "Invalid Timeframe" });
-    const serverList: { UUID: string; name: string; host: string; port: string }[] = JSON.parse((await redis.call("JSON.GET", "Minecraft:serverList", "$")) as string)[0];
-    if (!validateUUID(formatUUID(req.params.server)) || !serverList.some((s) => formatUUID(req.params.server) === s.UUID)) return res.status(422).json({ success: false, cause: "Invalid Server UUID" });
+    if (!servers.some((s) => req.params.server === s.ID)) return res.status(422).json({ success: false, cause: "Invalid Server ID" });
 
     res.set("Cache-Control", "public");
     if (req.params.timeframe === "hour") {
@@ -77,7 +75,7 @@ router.get("/v1/minecraft/server/:server/history/:timeframe", async (req, res) =
       res.set("Expires", date.toUTCString());
       return res.json({
         success: true,
-        data: formatTimeseries(await MinecraftServerPlayercountModel.shortTerm.find({ timestamp: { $gte: new Date(Date.now() - 60 * 60 * 1000), $lt: new Date() }, meta: formatUUID(req.params.server) }, ["-meta", "-_id", "-__v"]).lean()),
+        data: formatTimeseries(await MinecraftServerPlayercountModel.shortTerm.find({ timestamp: { $gte: new Date(Date.now() - 60 * 60 * 1000), $lt: new Date() }, meta: req.params.server }, ["-meta", "-_id", "-__v"]).lean()),
       });
     }
 
@@ -91,7 +89,7 @@ router.get("/v1/minecraft/server/:server/history/:timeframe", async (req, res) =
     if (req.params.timeframe === "year") startDate = new Date(Date.now() - 365 * 24 * 60 * 60 * 1000);
     return res.json({
       success: true,
-      data: formatTimeseries(await MinecraftServerPlayercountModel.longTerm.find({ timestamp: { $gte: startDate, $lt: new Date() }, meta: formatUUID(req.params.server) }, ["-meta", "-_id", "-__v"]).lean()),
+      data: formatTimeseries(await MinecraftServerPlayercountModel.longTerm.find({ timestamp: { $gte: startDate, $lt: new Date() }, meta: req.params.server }, ["-meta", "-_id", "-__v"]).lean()),
     });
   } catch (e) {
     Sentry.captureException(e);
