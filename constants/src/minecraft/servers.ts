74ccfb7c12e61f81afcaad02c27120d285037d9b
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-11-04 15:30:00 +0100

    Added `Constants`

---
+++ constants/src/minecraft/servers.ts
@@ -0,0 +1,152 @@
+export const MinecraftServerList = [
+  {
+    ID: "HYPIXEL",
+    host: "mc.hypixel.net",
+    name: "Hypixel",
+  },
+  {
+    ID: "WYNNCRAFT",
+    host: "play.wynncraft.com",
+    name: "Wynncraft",
+  },
+  {
+    ID: "GOMMEHD",
+    host: "play.gommehd.net",
+    name: "GommeHD",
+  },
+  {
+    ID: "CUBECRAFT",
+    host: "play.cubecraft.net",
+    name: "CubeCraft",
+  },
+  {
+    ID: "MINEHUT",
+    host: "minehut.gg",
+    name: "Minehut",
+  },
+  {
+    ID: "MINEMEN_CLUB",
+    host: "minemen.club",
+    name: "Minemen Club",
+  },
+  {
+    ID: "BEDWARS_PRACTICE_CLUB",
+    host: "bedwarspractice.club",
+    name: "Bedwars Practice Club",
+  },
+  {
+    ID: "BRIDGERLAND",
+    host: "bridger.land",
+    name: "BridgerLand",
+  },
+  {
+    ID: "CYTOOXIEN",
+    host: "cytooxien.de",
+    name: "Cytooxien",
+  },
+  {
+    ID: "MIXELPIXEL",
+    host: "play.mixelpixel.net",
+    name: "MixelPixel",
+  },
+  {
+    ID: "LANDANIA",
+    host: "landania.net",
+    name: "Landania",
+  },
+  {
+    ID: "MCC_ISLAND",
+    host: "play.mccisland.net",
+    name: "MCC Island",
+  },
+  {
+    ID: "ORIGIN_REALMS",
+    host: "play.originrealms.com",
+    name: "Origin Realms",
+  },
+  {
+    ID: "2B2T",
+    host: "2b2t.org",
+    name: "2b2t",
+  },
+  {
+    ID: "0B0T",
+    host: "0b0t.org",
+    name: "0b0t",
+  },
+  {
+    ID: "GRIEFERGAMES",
+    host: "griefergames.net",
+    name: "GrieferGames",
+  },
+  {
+    ID: "HYPERKNOX_WORLD",
+    host: "hyperknox.world",
+    name: "Hyperknox World",
+  },
+  {
+    ID: "OPSUCHT",
+    host: "opsucht.net",
+    name: "OPSUCHT",
+  },
+  {
+    ID: "HORSTBLOCKS",
+    host: "horstblocks.de",
+    name: "HorstBlocks",
+  },
+  {
+    ID: "BAUSUCHT",
+    host: "bausucht.net",
+    name: "BauSucht",
+  },
+  {
+    ID: "OG_NETWORK",
+    host: "og-network.net",
+    name: "OG Network",
+  },
+  {
+    ID: "ANTIAC",
+    host: "antiac.net",
+    name: "AntiAC",
+  },
+  {
+    ID: "TIMOLIA",
+    host: "timolia.de",
+    name: "Timolia",
+  },
+  {
+    ID: "PVP_LEGACY",
+    host: "play.pvplegacy.net",
+    name: "PVP Legacy",
+  },
+  {
+    ID: "PVP_GYM",
+    host: "pvpgym.net",
+    name: "PVP Gym",
+  },
+  {
+    ID: "MANACUBE",
+    host: "play.manacube.com",
+    name: "ManaCube",
+  },
+  {
+    ID: "SWACREW",
+    host: "swacrew.net",
+    name: "Swacrew",
+  },
+  {
+    ID: "MELVIN501",
+    host: "melvin501.de",
+    name: "Melvin501",
+  },
+  {
+    ID: "HOPLITE",
+    host: "hoplite.gg",
+    name: "Hoplite",
+  },
+  {
+    ID: "EARTHMC",
+    host: "earthmc.net",
+    name: "EarthMC",
+  },
+];
