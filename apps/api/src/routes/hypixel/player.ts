4bb057376849509a60b30b55e86d8da91e73ca56
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2023-11-16 18:39:22 +0100

    `API` Added latest `Hypixel/Wynncraft` Player data Endpoint

--- apps/api/src/routes/hypixel/player.ts
+++ apps/api/src/routes/hypixel/player.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import * as Sentry from "@sentry/node";
 import { parseUUID } from "@pixelic/mojang";
-import { HypixelHistoricalPlayerModel } from "@pixelic/mongo";
+import { HypixelHistoricalPlayerModel, HypixelPlayerModel } from "@pixelic/mongo";
 import { ratelimit } from "@pixelic/middlewares";
 
 const router = express.Router();
+
+router.get("/v1/hypixel/player/:player", ratelimit(), async (req, res) => {
+  const UUID = await parseUUID(req.params.player);
+  if (UUID === null) return res.status(422).json({ success: false, cause: "Invalid Player" });
+  try {
+    const data = await HypixelPlayerModel.findOne({ _id: UUID }, ["-_id", "-__v"]).lean();
+    if (Object.keys(data as object).length === 0) return res.status(404).json({ success: false, cause: "No Data Available" });
+
+    return res.json({ success: true, UUID, ...data });
+  } catch (e) {
+    Sentry.captureException(e);
+    return res.status(500).json({ success: false });
+  }
+});
 
 router.get("/v1/hypixel/player/:player/history", ratelimit(), async (req, res) => {
   const UUID = await parseUUID(req.params.player);
--- apps/api/src/routes/wynncraft/player.ts
+++ apps/api/src/routes/wynncraft/player.ts
@@ -1,10 +1,32 @@
 import express from "express";
 import * as Sentry from "@sentry/node";
 import { parseUUID } from "@pixelic/mojang";
-import { WynncraftHistoricalPlayerModel } from "@pixelic/mongo";
+import { WynncraftHistoricalPlayerModel, WynncraftPlayerModel } from "@pixelic/mongo";
 import { ratelimit } from "@pixelic/middlewares";
 
 const router = express.Router();
+
+router.get("/v1/wynncraft/player/:player", ratelimit(), async (req, res) => {
+  const UUID = await parseUUID(req.params.player);
+  if (UUID === null) return res.status(422).json({ success: false, cause: "Invalid Player" });
+  try {
+    const data = await WynncraftPlayerModel.findOne({ _id: UUID }, ["-_id", "-__v"]).lean();
+    if (Object.keys(data as object).length === 0) return res.status(404).json({ success: false, cause: "No Data Available" });
+
+    const characters: any = {};
+    for (const character of data?.characters || []) {
+      const characterUUID = character.UUID;
+      // @ts-ignore
+      delete character.UUID;
+      characters[characterUUID] = character;
+    }
+
+    return res.json({ success: true, UUID, ...data, characters });
+  } catch (e) {
+    Sentry.captureException(e);
+    return res.status(500).json({ success: false });
+  }
+});
 
 router.get("/v1/wynncraft/player/:player/history", ratelimit(), async (req, res) => {
   const UUID = await parseUUID(req.params.player);
